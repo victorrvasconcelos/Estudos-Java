b4e9bed2ad2e172db34d2d16e8a01add654fc58f
Victor Vasconcelos <victorvasconcelosdev@gmail.com>
2024-07-22 22:33:18 -0300

    Add files via upload

---
+++ estudos/dia 5/dia5/src/main/java/main.java
@@ -0,0 +1,23 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        int hora;
+        System.out.println("Quantas horas?");
+        hora = sc.nextInt();
+
+        if (hora < 12) {
+            System.out.println("Boa tarde");
+        }
+        else if (hora < 18) {
+            System.out.println("Boa tarde");
+        }
+        else {
+            System.out.println("Boa noite");
+        }
+        sc.close();
+    }
+}
