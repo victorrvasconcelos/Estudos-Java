7ada984f2638a76fd12e9349f3848acfa59557d6
Victor Vasconcelos <victorvasconcelosdev@gmail.com>
2024-08-27 22:35:03 -0300

    Add files via upload

---
+++ estudos/dia 32/dia32/src/main/java/org/example/Program.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import entities.BusinessAccount;
+
+public class Program {
+    public static void main(String[] args) {
+        BusinessAccount account = new BusinessAccount();
+
+        System.out.println(account);
+    }
+}
