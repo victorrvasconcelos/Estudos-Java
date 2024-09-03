17c5bdc5cadf7b1e83c1d8d4e046ce256701e0d8
Victor Vasconcelos <victorvasconcelosdev@gmail.com>
2024-09-02 22:25:13 -0300

    Add files via upload

---
+++ estudos/dia 35/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import entities.Account;
+import entities.SavingsAccount;
+import java.text.ParseException;
+
+public class Main{
+
+    public static void main(String[] args) throws ParseException {
+
+        Account x = new Account(1020, "Alex", 1000.0);
+        Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+
+        x.withdraw(50.0);
+        y.withdraw(50.0);
+
+        System.out.println(x.getBalance());
+        System.out.println(y.getBalance());
+
+    }
+}
