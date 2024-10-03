e385d13ae54dfe19a24ed3184539aec151514c71
Victor Vasconcelos <victorvasconcelosdev@gmail.com>
2024-10-02 22:35:09 -0300

    Add files via upload

---
+++ estudos/dia49/src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import entities.Client;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Client c1 = new Client("Maria", "maria@gmail.com");
+        Client c2 = new Client("Maria", "maria@gmail.com");
+
+        String s1 = new String("Test");
+        String s2 = new String("Test");
+
+        System.out.println(c1.hashCode());
+        System.out.println(c2.hashCode());
+        System.out.println(c1.equals(c2));
+        System.out.println(c1 == c2);
+        System.out.println(s1 == s2);
+
+
+
+    }
+}
